28b7afc7d3542baaf6ed0a5960b8edf0ded198e6
Your Name <jgbharris@hotmail.co.uk>
2021-04-13 09:35:08 +0100

    editing readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import Adapter from 'enzyme-adapter-react-15';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount} from 'enzyme';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 
-describe('<App />', () => {
-  it('renders dogContainer', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find("dogPicture"));
-  });
+it('renders without crashing', () => {
+  shallow(
+          <App />)
+});
+
+it('renders dogWrapper', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("dogWrapper"));
+});
+
+it('renders dogContainer', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("dogContainer"));
 });
 
 
